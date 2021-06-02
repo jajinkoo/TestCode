f9013cb2a4900d2cfe5663725d7efba009a84372
space0215@naver.com <space0215@naver.com>
2021-06-02 16:45:03 +0900

    c# 6.0 업데이트 테스트

--- Test1/Test1/Form1.cs
+++ Test1/Test1/Form1.cs
@@ -16,5 +16,49 @@ namespace Test1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e) // c# 6.0기능
+        {/*
+            ///1. ? 연산자 null 문자인지 확인하고 초기화 시키는명령어 
+            // int? cnt = 객체?.Count; // 카운트 객체가 null인제 체크 null 이면 cnt도 null로 된다. 
+
+            //. ?? 연산자
+            // int[] row = { 1,2,3};
+            // int ncnt = row[0]?.Count ?? 0; 
+
+
+            //2. 문자열 내샵 기능  (이건 유용한듯)
+            //$기능으로 string.format({0}) 이런식의 포맷 생략 시킬수 있다. 
+            Rectangle r = new Rectangle();
+            r.Height = 100;
+            r.Width = 100;
+
+            string str = $"{r.Height} * {r.Width} = {(r.Height * r.Width)}";
+
+            string str1 = string.Format("{0}*{1} = ({2})", r.Height, r.Width, r.Height * r.Width);
+
+            //3. Dictionary 초기화 방법 
+            //기존 
+            var scores = new Dictionary<string, int>()
+            {
+                { "kim", 100},
+                { "lee", 90}
+            };
+            int sc = scores["lee"];
+
+            //#6.0방법 
+            var scores2 = new Dictionary<string, int>()
+            {
+                ["kim"] = 100,
+                ["lee"] = 90
+            };
+            int sc2 = scores2["lee"];
+            */
+
+            var A = new[]{ 1, 2, 3 };
+
+            var L = new List<int>(A){ [2] = 9};
+
+        }
     }
 }
